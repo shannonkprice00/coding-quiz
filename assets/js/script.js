16967c934959b690ede9fa1ec54bdb50432d76da
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-03-17 13:52:50 -0500

    added logic to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,42 @@
 // Question Objects stored in Array
 var questionsArr = [{
-    question: "Which of the following is NOT a common data type stored in JavaScript?",
+    question: "1. Which of the following is NOT a common data type stored in JavaScript?",
     choices: ["A: Boolean","B: Number","C: Alert","D: String"],
     answer: "C: Alert"
 },{
-    question: "Inside which HTML element do we put the JavaScript?",
+    question: "2. Inside which HTML element do we put the JavaScript?",
     choices: ["A: <script>","B: <javascript>","C: <js>","D: <scripting>"],
     answer: "A: <script>"
 },{
-    question: "What is the correct JavaScript syntax to change the content of the following HTML element: <p id=”demo”>This is a demonstration.</p>",
+    question: "3. What is the correct JavaScript syntax to change the content of the following HTML element: <p id=”demo”>This is a demonstration.</p>",
     choices: ["A: document.getElement(“p”).innerHTML = “Hello World!”; ","B: document.getElementByName(“p”).innerHTML = “Hello World!”;","C: #demo.innerHTML = “Hello World!”;","D: document.getElementById(“demo”).innerHTML = “Hello World!”;"],
     answer: "D: document.getElementById(“demo”).innerHTML = “Hello World!”;",
 },{
-    question: "Where is the correct place to insert a JavaScript?",
+    question: "4. Where is the correct place to insert a JavaScript?",
     choices: ["A: Both the <head> section and the <body> section are correct","B: The <head> section","C: The <head> section","D: The <meta> section"],
     answer: "A: Both the <head> section and the <body> section are correct",
 },{
-    question: "How do you write “Hello World” in an alert box?",
+    question: "5. How do you write “Hello World” in an alert box?",
     choices: ["A: msgBox(“Hello World”);","B: alert(“Hello World”);","C: alertBox(“Hello World”);","D: msg(“Hello World”);"],
     answer: "B: alert(“Hello World”);",
 },{
-    question: "How do you create a function in JavaScript?",
+    question: "6. How do you create a function in JavaScript?",
     choices: ["A: function myFunction()","B: function = myFunction()","C: function:myFunction()","D: function myFunction"],
     answer: "A: function myFunction()",
 },{
-    question: "How do you call a function named “myFunction”?",
+    question: "7. How do you call a function named “myFunction”?",
     choices: ["A: call myFunction();","B: call function myFunction();","C: myFunction();","D: myFunction;"],
     answer: "C: myFunction();",
 },{
-    question: "How do you write an IF statement in JavaScript?",
+    question: "8. How do you write an IF statement in JavaScript?",
     choices: ["A: if i = 5","B: if i == 5 then","C: if (i == 5) ","D: if i = 5 then"],
     answer: "C: if (i == 5) ",
 },{
-    question: "How do you write an IF statement for executing some code if “i” is NOT equal to 5?",
+    question: "9. How do you write an IF statement for executing some code if “i” is NOT equal to 5?",
     choices: ["A: if (i != 5)","B: if i =! 5 then","C: if (i <> 5)","D: if i <> 5"],
     answer: "A: if (i != 5)",
 },{
-    question: "How does a FOR loop start?",
+    question: "10. How does a FOR loop start?",
     choices: ["A: for (i <= 5; i++)","B: for (i = 0; i <=5)","C: for (i = 0; i <= 5; i++)","D: for i = 1 to 5"],
     answer: "C: for (i = 0; i <= 5; i++)",
 }]
@@ -44,12 +44,14 @@ var questionsArr = [{
 var initialPageDiv = document.querySelector("#initial-page");
 var timerEl = document.querySelector("#timer");
 var scoreEl = document.querySelector("#high-scores");
+var scoreDiv = document.querySelector("#score-div");
+var timeDiv = document.querySelector("#time-div");
 var startBtn = document.querySelector("#start-btn");
 var questionSctn = document.querySelector("#question-sctn");
 var endSection = document.querySelector("#end");
 var highScores = document.querySelector("#view-high-scores");
 
-// Other Variables Needed
+// Other Variables Needed in Global Scope
 var timerCount = 0;
 var score = 0;
 var currentQuestionIndex = 0;
@@ -58,10 +60,15 @@ var scoresArr = [];
 
 
 // Functions
+function init() {
+    scoreDiv.style.visibility = "hidden";
+    timeDiv.style.visibility = "hidden";
+    console.log(questionsArr);
+}
+
 function nextQuestion(event) {
-// check answers here
-var selectedAnswer = event.target.innerHTML;
-console.log(selectedAnswer)
+    // check answers here
+    var selectedAnswer = event.target.textContent;
     if(selectedAnswer === questionsArr[currentQuestionIndex].answer) {
         var hrEl = document.createElement("hr");
         var correctEl = document.createElement("p");
@@ -85,9 +92,8 @@ console.log(selectedAnswer)
             endQuiz()
         } else {
             renderQuestion();
-            renderScore();
         }
-    }, 3000)
+    }, 2000)
 };
 
 function renderQuestion() {
@@ -112,6 +118,10 @@ function renderQuestion() {
     listEl.appendChild(lineEl3);
     listEl.appendChild(lineEl4);
 
+    // lineEl1.addEventListener("click", function () {
+    //     // .disable = true
+    //     nextQuestion();
+    // });
     lineEl1.addEventListener("click", nextQuestion);
     lineEl2.addEventListener("click", nextQuestion);
     lineEl3.addEventListener("click", nextQuestion);
@@ -120,7 +130,7 @@ function renderQuestion() {
 
 function renderScore() {
     var currentScore = document.querySelector("#current-score");
-    currentScore.textContent = score.valueOf();
+    currentScore.textContent = score.valueOf() + " %";
 }
 
 function startTimer() {
@@ -135,49 +145,63 @@ function startTimer() {
 }
 
 function startQuiz() {
-    timerCount = 50;
+    timeDiv.style.visibility = "visible";
+    timerCount = 75;
     startTimer();
     initialPageDiv.style.display = "none";
     renderQuestion();
-    renderScore();
 }
 
 function viewHighScores() {
     endSection.style.display = "none";
     questionSctn.style.display = "none";
+    initialPageDiv.style.display = "none";
+
+    // if ("userScore" === '') {
+    //     var msg = document.createElement("p");
+    //     msg.textContent = "No Scores Yet";
+    //     endSection.appendChild(msg);
+    // }
     var scoreDisplay = JSON.parse(localStorage.getItem("userScore"));
     for (var i = 0; i < scoreDisplay.length; i++) {
         var scoreDisplayli = document.createElement("li");
-        scoreDisplayli.innerHTML = "Initials: " + scoreDisplay.initials[i] + "Score: " + scoreDisplay.score[i];
-        console.log(scoreDisplayli);
+        scoreDisplayli.innerHTML = "Initials: " + scoreDisplay[i].initials + " Score: " + scoreDisplay[i].score + " %";
         highScores.appendChild(scoreDisplayli);
-}
+    }
 }
 
 function endQuiz() {
-    questionSctn.style.display = "none"
-    var outcome = document.createElement("h1")
-    outcome.innerHTML = "You got a score of: " + score + "/10"
-    endSection.append(outcome)
-    var input = document.createElement("input")
-    input.setAttribute("placeholder", "Initials...")
-    endSection.append(input)
-    var submit = document.createElement("button")
-    submit.innerHTML = "Submit"
-    endSection.append(submit)
+    questionSctn.style.display = "none";
+    var outcome = document.createElement("h1");
+    outcome.innerHTML = "You got a score of: " + (score / 10) + "/10";
+    endSection.append(outcome);
+    var input = document.createElement("input");
+    input.setAttribute("placeholder", "Initials...");
+    endSection.append(input);
+    var submit = document.createElement("button");
+    submit.innerHTML = "Submit";
+    endSection.append(submit);
     submit.addEventListener("click", function() {
 
-    var intials = input.value;
+    var initials = input.value;
     var values = {
-        intials: intials,
+        initials: initials,
         score: score
     };
     scoresArr.push(values);
     localStorage.setItem("userScore", JSON.stringify(scoresArr));
-
-    viewHighScores();
+    input.setAttribute("placeholder", " ");
 })
+    renderScore();
 }
+init ();
 
 startBtn.addEventListener("click", startQuiz);
 scoreEl.addEventListener("click", viewHighScores);
+
+// TO DO: disable event listeners so items may only be clicked once (MC items, view high score element, and submit button on end page)
+// TO DO: endQuiz functions once when timer hits 0 (or less due to time deduction penalties) AND when finished with last question...I need
+// to figure out how to make the score and initials input box only appear one time if the user gets through all the questions. 
+// TO DO: figure out how to get high scores to push arrays everytime function is called.
+// TO DO: add "play again" and "view high score" buttons to end page (after initials are submitted) 
+// TO DO: style project
